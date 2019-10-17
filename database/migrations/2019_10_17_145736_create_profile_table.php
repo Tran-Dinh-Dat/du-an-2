1a3f180a1361882643dd5ef009cc8dd230ecd436
Tran-Dinh-Dat <trandinhdat120@gmail.com>
2019-10-17 22:55:40 +0700

    migrate

---
+++ database/migrations/2019_10_17_145736_create_profile_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('username');
+            $table->string('fullname');
+            $table->string('avatar');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
