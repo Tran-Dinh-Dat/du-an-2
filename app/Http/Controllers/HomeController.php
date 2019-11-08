1e3a5c5d69dfa0d81f1ab76e5a32ad82789faecc
Tran-Dinh-Dat <trandinhdat120@gmail.com>
2019-11-08 10:00:37 +0700

    show categories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Category;
 class HomeController extends Controller
 {
     /**
@@ -23,7 +23,8 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+        $categories = Category::all();
         $request->user()->authorizeRoles(['user', 'admin']);
-        return view('frontend.index');
+        return view('frontend.index', compact('categories'));
     }
 }
